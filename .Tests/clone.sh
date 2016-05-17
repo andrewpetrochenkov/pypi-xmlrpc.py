34ed1133735e4bab98e99bb4656017fe3608e944
russianidiot <russianidiot.github@gmail.com>
2016-05-18 02:33:32 +0300

    +.Tests/url.txt; ^.Tests/clone.sh,.coveragerc; -.Tests/clone/github.sh

--- .Tests/clone.sh
+++ .Tests/clone.sh
@@ -5,22 +5,8 @@
 echo "DEBUG: ~/.Tests/ NOT EXISTS, continue"
 [[ -z "$TESTS_URL" ]] && {
 	echo "DEBUG: \$TESTS_URL NOT DEFINED"
-	origin="$(git config remote.origin.url)" || exit
-	[[ -z $origin ]] && {
-		( set -x; git config remote.origin.url )
-		echo "ERROR: remote UNKNOWN"
-		exit 1
-	}
-
-	clone="${BASH_SOURCE[0]%/*}"/clone
-	[[ "$origin" == *"github.com:"* ]] || [[ "$origin" == *"github.com/"* ]] && {
-		{ set -x; . "$clone"/github.sh || exit; { set +x; } 2>/dev/null; }
-	}
-	# todo: bitbucket, gitlab, etc
-	[[ -z "$TESTS_URL" ]] && {
-		echo "$origin NOT SUPPORTED"
-		{ set -x; . "${BASH_SOURCE[0]%/*}"/config.sh; { set +x; } 2>/dev/null; }
-	}
+	txt="${BASH_SOURCE[0]%/*}/url.txt"
+	TESTS_URL="$(set -x; cat "$txt")" || exit
 }
 
 ( set -x; git clone --depth 1 "$TESTS_URL" ~/.Tests ) || exit
