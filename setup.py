73a1f2537f6837d701c3c6620142989568db8309
russianidiot <russianidiot.github@gmail.com>
2016-08-26 03:26:31 +0300

    ^README.md,requirements.txt,setup.py,version.txt

--- setup.py
+++ setup.py
@@ -8,11 +8,13 @@ except ImportError:
 kwargs = dict()
 
 # known-issues:
-# pip attempts to discover all of the dependencies before installation
-# python setup.py egg_info
-install_requires = []
+# pip running `python setup.py egg_info` before installation:
+# 1) pip checks metadata name pip/req/req_install.py:run_egg_info()
+# 2) pip attempts to discover all of the dependencies before installation
+name = os.path.basename(os.getcwd()).split(".")[0].lower()
+
 path = os.path.join(os.getcwd(), "requirements.txt")
 if os.path.exists(path) and os.path.isfile(path):
     kwargs["install_requires"] = open(path).read().splitlines()
 
-setup(**kwargs)
+setup(name=name,**kwargs)
